ed0c980cda42d9280fb5d918c83ff725d9911280
Alexsis Moskovchuk <alekssiuss@gmail.com>
2021-10-12 22:06:33 +0300

    hello mates

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -7,6 +7,7 @@ public class Main {
 
     public static void main(String[] args) {
 
+        System.out.println("Hello Mates!");
         Random random = new Random();
         System.out.println(random.nextDouble());
     }
